bf33852b10613c3345466a68e679569753b11f9b
goooooosha789 <152414790+goooooosha789@users.noreply.github.com>
2024-04-17 21:14:43 +0300

    Add files via upload

---
+++ data/users.py
@@ -0,0 +1,10 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    favorites = sqlalchemy.Column(sqlalchemy.String, nullable=True, default='Здесь ничего нет')
+
