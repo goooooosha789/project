1990fd38194018b477421dbc90deb69dccb4efe6
goooooosha789 <152414790+goooooosha789@users.noreply.github.com>
2024-04-27 17:04:02 +0300

    Update users.py

--- data/users.py
+++ data/users.py
@@ -6,5 +6,5 @@ class User(SqlAlchemyBase):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    favorites = sqlalchemy.Column(sqlalchemy.String, nullable=True, default='Здесь ничего нет')
-
+    favorites = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    last_search = sqlalchemy.Column(sqlalchemy.String, nullable=True)
